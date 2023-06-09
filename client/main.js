b13c1a198027e5d50e4b8986ef3723b65bf88ca6
Kenzo Aaron Dela Cruz <kenzo.delacruz03@gmail.com>
2023-06-09 13:26:49 +0800

    Ongoing Enrollment Page

--- client/main.js
+++ client/main.js
@@ -1,3 +1,5 @@
+//Login Page
+
 const loginForm = document.getElementById("login-form");
 const loginButton = document.getElementById("login-form-submit");
 const loginErrorMsg = document.getElementById("login-error-msg");
@@ -9,8 +11,50 @@ loginButton.addEventListener("click", (e) => {
 
     if (username === "user" && password === "web_dev") {
         alert("You have successfully logged in.");
-        location.reload();
+        location.href = './enrollment.html';
     } else {
         loginErrorMsg.style.opacity = 1;
     }
 })
+
+
+
+//Enrollment page
+
+function showForm() {
+  var selopt = document.getElementById("opts").value;
+  if (selopt == 1) {
+      document.getElementById("f1").style.display = "block";
+      document.getElementById("f2").style.display = "none";
+      document.getElementById("f3").style.display = "none";
+      document.getElementById("f4").style.display = "none";
+  }
+  if (selopt == 2) {
+      document.getElementById("f2").style.display = "block";
+      document.getElementById("f1").style.display = "none";
+      document.getElementById("f3").style.display = "none";
+      document.getElementById("f4").style.display = "none";
+  }
+  if (selopt == 0) {
+      document.getElementById("f2").style.display = "none";
+      document.getElementById("f1").style.display = "none";
+      document.getElementById("f4").style.display = "none";
+  }
+  if (selopt == 3) {
+    document.getElementById("f2").style.display = "none";
+    document.getElementById("f1").style.display = "none";
+    document.getElementById("f3").style.display = "block";
+    document.getElementById("f4").style.display = "none";
+}
+
+  if (selopt == 4) {
+    document.getElementById("f2").style.display = "none";
+    document.getElementById("f1").style.display = "none";
+    document.getElementById("f3").style.display = "block";
+    document.getElementById("f4").style.display = "block";
+  }
+}
+
+
+
+//Homepage
